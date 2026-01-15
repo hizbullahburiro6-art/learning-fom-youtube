5295f5f4dddca8c6b4a54888549423d0cf124e42
hizbullahburiro6-art <hizbullahburiro6@gmail.com>
2026-01-15 22:41:40 +0500

    fix:

---
+++ script.js
@@ -0,0 +1,65 @@
+// assiment operator
+
+// let a = 5;
+// let b = 4;
+
+// a **= 4;
+// console.log(a)
+
+
+//comprative operator
+
+
+// let a = 5;
+// let b    4;
+
+// console.log(a == b)
+
+
+
+// let mode = 'yellow';
+// let color;
+
+// if(mode === 'dark'){
+//     color = 'black';
+
+// }else if (mode === 'blue'){
+//     color = 'blue'
+// }else if (mode === 'pink'){
+//     color = 'pink'
+// }else{
+//     color = 'white'
+// }
+
+// console.log(color)
+
+// let num = prompt("Enter a number ");
+// if(num % 5 === 0){
+//     console.log(num, " is a multiple of 5")
+// }else {
+//     console.log(num, "num is not multiple of 5")
+// }
+
+
+// let marks =48;
+// let grade;
+
+// if(marks >= 90){
+//     grade = "A"
+// }else if(marks >= 70){
+//     grade = "B"
+// }else if(marks >= 60){
+//     grade = "C"
+// }else if(marks >= 50){
+//     grade = "D"
+// }else if (marks >= 0){
+//     grade = "F"
+// }
+// console.log("according to your score ,your grade was " ,grade)
+
+
+for ( let number = 1; number <= 10; number++){
+    console.log("Hizbullah Buriro")
+}
+
+console.log("lops has been ended")
